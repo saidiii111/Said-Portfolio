d96b627c60b9d5bd2fd1e40a73ea3c70eeb5c558
said <alsaghir.said@gmail.com>
2018-09-12 14:32:25 +0200

    adding bkg img to about page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react';
 
 import bewer from '../img/bewer.jpg';
+import beachCloud from '../img/beachCloud.jpg';
 
 class About extends Component{
   componentDidMount(){
@@ -10,9 +11,12 @@ class About extends Component{
     return(
   <Fragment>
       <main className="ml-3">
+        <div className="box">
+          <img className="img-responsive" src={beachCloud} alt="beachCloud" />
+           <div className="img-title">
     <section>
-       <h1>My Résumé:</h1>
-       <dt> Name:</dt>
+       <h1>My Résumé</h1>
+       <dt> Name</dt>
        <dd className="lead">Said SAGHIR</dd>
        <img className="d-inline  rounded-circle" src={bewer} alt="Said" width="150" height="200" />
      </section>
@@ -23,9 +27,13 @@ class About extends Component{
        </div>
      </section>
      <section>
+        <h2> IT Skills:</h2>
+        <h1 className="lead"> React, Redux, Cisco Networks, MS Office, HTML5, CSS3, Sass, Scss, JAVA-SCRIPT</h1>
+     </section>
+     <section>
        <h2>  Languages:</h2>
        <dt>Arabic:</dt>
-       <dd>Mother-language</dd>
+       <dd>Nativ-language</dd>
        <dt>German:</dt>
        <dd>Good</dd>
        <dt> Norwigen: </dt>
@@ -34,10 +42,10 @@ class About extends Component{
        <dd> Good</dd>
        <dt>English</dt>
        <dd>Good</dd>
-       <h2> IT Skills:</h2>
-       <p className="lead"> React, Redux, Cisco Networks, MS Office, HTML5, CSS3, Sass, Scss, JAVA-SCRIPT</p>
-       <p>Hobbies: Swimming, Reading, Natur Berlin, 03.09.2018 Said Saghir</p>
+       <h4> Berlin, 03.09.2018 Said Saghir</h4>
      </section>
+   </div>
+   </div>
    </main>
  </Fragment>
    );
